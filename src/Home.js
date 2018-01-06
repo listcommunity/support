e64ec3dc3abd7181bccbe5abd6ebb742153775f7
David Francisco <dmfrancisc@gmail.com>
2018-01-06 17:22:26 +0000

    Design changes to the homepage

--- src/Home.js
+++ src/Home.js
@@ -3,32 +3,44 @@ import { Link } from "react-router-dom";
 import { Helmet } from "react-helmet";
 import groupBy from "lodash.groupby";
 
-import Topbar from "./Topbar";
+import Jungle from "./Jungle";
+import { GitHubIcon } from "./Icon";
 import octomonkey from "./octomonkey.svg";
 import allLists from "./lists";
 
 const Home = () => (
-  <div className="min-h-screen bg-grey-lighter font-sans text-black tracking-tight">
-    <Topbar />
+  <Jungle className="bg-white font-sans text-purple-darker tracking-tight">
+    <div className="max-w-xl mx-auto mb-6 pt-15">
+      <div className="px-4 max-w-lg xl:max-w-full flex flex-wrap flex-row-reverse mx-auto flex items-center mb-8">
+        <div className="w-full lg:w-1/3">
+          <img
+            src={octomonkey}
+            draggable="false"
+            className="select-none block mx-auto mb-8 lg:mb-0 lg:max-w-full xl:w-full lg:pl-4"
+            alt="Octomonkey welcomes you!"
+          />
+        </div>
 
-    <div className="max-w-xl mx-auto text-center">
-      <img
-        src={octomonkey}
-        width="190"
-        height="190"
-        draggable="false"
-        className="mt-12"
-        alt="Octomonkey welcomes you!"
-      />
+        <div className="w-full lg:w-2/3">
+          <h2
+            className="text-2xl sm:text-5xl leading-tight tracking-tight mb-6 text-center lg:text-left mx-auto lg:mx-0"
+            style={{ maxWidth: 520 }}>
+            Navigate through the <br /> information jungle
+          </h2>
 
-      <p className="text-xl mx-auto leading-normal pb-6" style={{ maxWidth: 480 }}>
-        Welcome to List.community, a website that lets you browse and search awesome lists more
-        easily.
-      </p>
+          <h3
+            className="text-lg sm:text-3xl leading-normal tracking-tight text-purple-darkest font-normal text-center lg:text-left mx-auto lg:mx-0"
+            style={{ maxWidth: 480 }}>
+            List.community is an easy way to <br /> browse curated lists on GitHub.
+          </h3>
+        </div>
+      </div>
 
       {Object.entries(groupBy(allLists, "category")).map(([category, lists]) => (
         <Fragment key={category}>
-          <h2 className="py-6 text-xl">{category}</h2>
+          <h2 className="py-6 text-xl px-4 text-center lg:text-left uppercase tracking-normal">
+            {category}
+          </h2>
 
           <ul className="list-reset clearfix pb-6">
             {lists.map(list => (
@@ -57,10 +69,27 @@ const Home = () => (
       ))}
     </div>
 
+    <header className="fixed z-20 pin-t pin-x select-none antialiased text-white bg-white shadow hidden sm:block">
+      <div className="max-w-xl mx-auto flex items-center h-16">
+        <Link to="/" className="link-reset p-4">
+          <h1 className="text-2xl font-bold tracking-tight text-purple-darker">List.community</h1>
+        </Link>
+
+        <div className="flex-1" />
+
+        <a
+          href={"https://github.com/listcommunity/support"}
+          className="link-reset px-4 py-3 mr-4 bg-purple-dark rounded items-center text-lg flex">
+          <span className="mr-3">Submit a list</span>
+          <GitHubIcon width="16" height="16" />
+        </a>
+      </div>
+    </header>
+
     <Helmet>
       <link rel="icon" type="image/png" href="/favicon.png" />
     </Helmet>
-  </div>
+  </Jungle>
 );
 
 export default Home;
