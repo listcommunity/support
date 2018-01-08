14b2f2e550535ee490ec39271a7b7b22e31bf7df
David Francisco <dmfrancisc@gmail.com>
2018-01-08 00:42:03 +0000

    Open access token page on a new tab

--- src/AccessTokenDialog.js
+++ src/AccessTokenDialog.js
@@ -18,31 +18,35 @@ class AccessTokenDialog extends Component {
         <div className="fixed bg-black pin" style={{ opacity: 0.9 }} />
 
         <div className="relative mx-auto max-w-md">
-        <div className="my-6 mx-2 shadow-lg p-6 bg-white rounded leading-loose text-lg">
-          <p>
-            GitHub limits unauthenticated API requests to 60 per hour, but after binding your access
-            token you will get a limit of 5,000 requests per hour.
-          </p>
-          <p className="mt-2">
-            <a href="https://github.com/settings/tokens/new" className="text-blue">
-              Create a new access token
-            </a>{" "}
-            without any scopes and paste it back here:
-          </p>
-          <div className="flex mt-4">
-            <input
-              ref={input => (this.input = input)}
-              type="text"
-              placeholder="Paste access token here"
-              className="flex-1 p-2 border mr-2 rounded-sm"
-            />
-            <button
-              onClick={this.handleSubmit}
-              className="py-2 px-4 flex-none bg-blue rounded-sm shadow bg-blue hover:bg-blue-dark text-white">
-              Save
-            </button>
+          <div className="my-6 mx-2 shadow-lg p-6 bg-white rounded leading-loose text-lg">
+            <p>
+              GitHub limits unauthenticated API requests to 60 per hour, but after binding your
+              access token you will get a limit of 5,000 requests per hour.
+            </p>
+            <p className="mt-2">
+              <a
+                href="https://github.com/settings/tokens/new"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-blue">
+                Create a new access token
+              </a>{" "}
+              without any scopes and paste it back here:
+            </p>
+            <div className="flex mt-4">
+              <input
+                ref={input => (this.input = input)}
+                type="text"
+                placeholder="Paste access token here"
+                className="flex-1 p-2 border mr-2 rounded-sm"
+              />
+              <button
+                onClick={this.handleSubmit}
+                className="py-2 px-4 flex-none bg-blue rounded-sm shadow bg-blue hover:bg-blue-dark text-white">
+                Save
+              </button>
+            </div>
           </div>
-        </div>
         </div>
       </div>
     );
