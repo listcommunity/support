4783b145c7ce6d69af9a878800b2965ec0d391c5
David Francisco <dmfrancisc@gmail.com>
2018-01-16 00:08:16 +0000

    Fix topbar alignment when list name is long

--- src/Topbar.js
+++ src/Topbar.js
@@ -26,31 +26,31 @@ class Topbar extends Component {
             <h1 className="text-xl font-semibold font-sans tracking-wide">List.community</h1>
           </Link>
 
-          <div className="text-xl hidden xl:block">{fullName}</div>
+          <div className="text-xl hidden xl:block truncate">{fullName}</div>
 
           <div className="flex-1" />
 
           <a
             key="submit"
             href={`https://github.com/${fullName}`}
-            className="link-reset p-3 pt-4 hidden sm:block">
+            className="link-reset p-3 pt-4 hidden sm:block flex-none">
             Submit a resource
           </a>
           <a
             key="curators"
             href={`https://github.com/${fullName}/graphs/contributors`}
-            className="link-reset p-3 pt-4 hidden sm:block">
+            className="link-reset p-3 pt-4 hidden sm:block flex-none">
             Curators
           </a>
 
-          <a href={`https://github.com/${fullName}`} className="link-reset p-3 pt-4 leading-none">
+          <a href={`https://github.com/${fullName}`} className="link-reset p-3 pt-4 leading-none flex-none">
             <GitHubIcon width="16" height="16" />
           </a>
 
           {sidebarOpen === false && (
             <div
               onClick={this.openSidebar}
-              className="cursor-pointer lg:hidden p-3 pt-4 leading-none">
+              className="cursor-pointer lg:hidden p-3 pt-4 leading-none flex-none">
               <HamburguerIcon width="16" height="16" />
             </div>
           )}
@@ -60,7 +60,7 @@ class Topbar extends Component {
               onClick={this.closeSidebar}
               className={`${
                 sidebarOpen ? "" : "hidden"
-              } cursor-pointer lg:hidden p-3 pt-4 leading-none`}>
+              } cursor-pointer lg:hidden p-3 pt-4 leading-none flex-none`}>
               <CloseIcon width="16" height="16" />
             </div>
           )}
