91a6d749abd9fc4fbecb830542b23b288ad79298
David Francisco <dmfrancisc@gmail.com>
2018-01-25 00:49:34 +0000

    Fix broken relative links to SVGs in readmes

--- src/CustomImg.js
+++ src/CustomImg.js
@@ -7,7 +7,7 @@ const CustomImg = ({ children, ...props }) => {
   let src =
     isAbsoluteUrl(props.src) || props.src.startsWith("//") || props.src.startsWith("#")
       ? props.src
-      : urlJoin("https://github.com", window.location.pathname, "/raw/master/", props.src);
+      : urlJoin("https://github.com", window.location.pathname, "/raw/master/", props.src, "?sanitize=true");
 
   if (isAbsoluteUrl(src) || src.startsWith("//")) {
     moreProps = { target: "_blank", rel: "noopener noreferrer" };
